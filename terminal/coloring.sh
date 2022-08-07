c6a98b5df879b3c13fb09193934a9acf7f0de594
Rıza Kaan Uçak <rzakaan@gmail.com>
2022-08-07 13:26:44 +0300

    new file added

---
+++ terminal/coloring.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+if [[ $OSTYPE == 'darwin'* ]]; then
+    ZPROFILE="/Users/$USER/.zprofile"
+    echo "export CLICOLOR=1" >> $ZPROFILE
+    echo "export LSCOLORS=GxFxCxDxBxegedabagaced" >> $ZPROFILE
+else
+    # bash profile
+    export TERM="xterm-color" 
+    export PS1='\[\e[0;33m\]\u\[\e[0m\]@\[\e[0;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w\[\e[0m\]\$ '
+fi
